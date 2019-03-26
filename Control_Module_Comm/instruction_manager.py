72067acf0253f11278de95d803bc72a0f57a795a
EdgardoAndres <EdgardooAndres@users.noreply.github.com>
2019-03-26 02:10:51 -0400

    # Done - [x] Formatted Code and Re-organized Project Structure.
    # To-Do
    * [ ] Initialize Widgets.
    * [ ] Code Basic Functionality to GUI Widgets and Components.
    * [ ] Link GUI components to Control Module Communication Classes.
    
    # Bugs
    * [ ] $ Bug

--- Control_Module_Comm/instruction_manager.py
+++ Control_Module_Comm/instruction_manager.py
@@ -1,16 +1,19 @@
-import serial_interface
+from Control_Module_Comm import serial_interface
 
 log = 1
+
+
 class instruction_manager():
     def init(self, listen):
         self.serial_interface = serial_interface.serial_interface()
         self.listen = listen
-        
+
     """
     function to set configuration.
     input: string with configuration
     sends an instruction byte to know what it must do, and then the configuration as a string
     """
+
     def send_set_configuration(self, string):
         self.serial_interface.send_byte(b'\x80')
         self.serial_interface.send_string(string)
@@ -19,10 +22,11 @@ class instruction_manager():
         if line == b'\xFF':
             if log: print("send test succesful")
             return 1
-        
+
     """
     requests configuration
     """
+
     def send_request_configuration(self):
         self.serial_interface.send_instruction(b'\x81')
         line = self.serial_interface.listen()
@@ -31,10 +35,11 @@ class instruction_manager():
             if log: print("send request configuration succesful")
             line = self.serial_interface.listen()
             line = line.strip(b'\r\n')
-    
+
     """
     send an instruction byte to request start 
     """
+
     def send_request_start(self):
         self.serial_interface.send_instruction(b'\x82')
         line = self.serial_interface.listen()
@@ -42,10 +47,11 @@ class instruction_manager():
         if line == b'\xFF':
             if log: print("send request start succesful")
             return 1
-    
+
     """
     this is for requesting the number of modules that the device has connected at any one time. it returns 
     """
+
     def send_request_number_of_mods_connected(self):
         self.serial_interface.send_instruction(b'\x84')
         line = self.serial_interface.listen()
@@ -54,10 +60,10 @@ class instruction_manager():
             if log: print("send modules connected succesful")
             line = self.serial_interface.listen()
             line = str(line)
-            line = line[4:len(line)-5]
+            line = line[4:len(line) - 5]
             line = line.split("\\x")
             return line
-        
+
     def send_live_stream_request(self, module, channel1, channel2):
         self.serial_interface.send_byte(b'\x88')
         info = str(module) + str(channel1) + str(channel2)
@@ -66,7 +72,7 @@ class instruction_manager():
         line = line.strip(b'\r\n')
         if line == b'\xFF':
             if log: print("send live stream request succesful")
-            
+
     def send_cancel_request(self):
         self.serial_interface.send_instruction(b'\xFF')
         line = self.serial_interface.listen()
---
+++ Control_Module_Comm/serial_output.py
@@ -0,0 +1,15 @@
+import serial
+
+ser = serial.Serial(
+    port='COM6',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1)
+
+print("connected to: " + ser.portstr)
+count = 1
+
+ser.write(b'hola\r')
+print("---test end---")
