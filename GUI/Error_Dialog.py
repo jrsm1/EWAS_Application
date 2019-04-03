e73c40cecddf6d442ac2f3739672671791b0b759
EdgardoAndres <EdgardooAndres@users.noreply.github.com>
2019-04-03 08:49:41 -0400

    # Done - [x] Created GUI Handling Error Dialog from given button for TESTING.
    # To-Do
    * [ ]
    
    # Bugs
    * [ ] $ Bug

---
+++ GUI/Error_Dialog.py
@@ -0,0 +1,5 @@
+import GUI
+
+
+gui = GUI.GUI_Components
+gui.show_error('Error Mssg')
--- GUI/__init__.py
+++ GUI/__init__.py
@@ -4,10 +4,27 @@ from PyQt5 import QtWidgets, uic
 app = QtWidgets.QApplication([])
 main_window = uic.loadUi("main_tab_layout.ui")
 channel_info_win = uic.loadUi("channel_info_window.ui")
-err_dlg = uic.loadUi("error_dialog_v1.ui")
+# err_dlg = uic.loadUi("error_dialog_v1.ui")
 prog_dlg = uic.loadUi("progress_dialog_v1.ui")
 
 
+def start():
+    main_window.main_advanced_sample_rate_label.setText("NEW TEXT")
+
+"""
+    Creates an Error Message dialog
+    :param message: String - The Desired Message Output.
+"""
+def show_error(message: str):
+    err_dlg = QtWidgets.QErrorMessage()
+    err_dlg.showMessage(message)
+    err_dlg.exec()
+"""
+Add default functionality here
+"""
+
+main_window.main_advanced_HELP_button.clicked.connect(lambda: show_error('This is an Error Message Dialig Box. \n You have done Something Wrong.'))
+
 main_window.show()
-err_dlg.show()
 app.exec()
+
