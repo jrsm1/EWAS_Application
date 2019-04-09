0cea908980f825890a6dcdd2c2c358c329869ce4
EdgardoAndres <EdgardooAndres@users.noreply.github.com>
2019-04-09 09:28:18 -0400

    - [X] Creates Handler for saving and loading setting profile. * [ ] Change/Create Filename method to save in proper path * [ ] Handle Better Test Names.

--- Control_Module_Comm/Structures/Channel_Individual.py
+++ Control_Module_Comm/Structures/Channel_Individual.py
@@ -4,5 +4,11 @@ class Channel:
     def __init__(self, channel_name: str):
 
         self.channel_info = {
-         'channel_name' : channel_name,
+         'channel_name': channel_name,
         }
+
+
+# TESTING
+# ch = Channel('mTestChannel')
+# ch.channel_info['channel_name'] = 'mNewName'
+# print(ch.channel_info['channel_name'])
--- Control_Module_Comm/Structures/Sensor_Individual.py
+++ Control_Module_Comm/Structures/Sensor_Individual.py
@@ -7,15 +7,18 @@ channel_info = {
 
 class Sensor:
     # TODO VERIFY IS CASE WHERE SOMETHING IS EMPTY IS POSSIBLE. --> SET DEFAULT VALUES.
+    # TODO add default case.
     def __init__(self, sensor_name: str, sensor_description: str, sensor_sensitivity: str, sensor_localization: str):
         self.sensor_info = {
-            "given_name": sensor_name,
-            "sensitivity": sensor_sensitivity,
+            "sensor_name": sensor_name,
             "description": sensor_description,
+            "sensitivity": sensor_sensitivity,
             'localization': sensor_localization
         }
 
 
 # TESTING
-sens = Sensor('My Sensor', 'No Description', '45 mV/g', "location Null")
-print(sens.sensor_info)
+# sens = Sensor('My Sensor', 'No Description', '45 mV/g', "location Null")
+# sens.sensor_info['sensor_name'] = 'mNewName'
+# sens.sensor_info['sensitivity'] = 'mNewSensi'
+# print(sens.sensor_info)
