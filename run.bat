fa0b1679a10f49977687b4760c25f9751cf6c0ba
EdgardoAndres <EdgardooAndres@users.noreply.github.com>
2019-05-21 11:32:35 -0400

    [X] edited batch file

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-"C:\Users\drgdm\AppData\Local\Programs\Python\Python37\python.exe" "C:\Users\drgdm\OneDrive\Documents\GitHub\EWAS_Application\main.py"
+"C:\Users\drgdm\AppData\Local\Programs\Python\Python37\python.exe" "C:\Users\[YOUR USERNAME]\[YOUR PATH]\EWAS_Application\main.py"
 pause
