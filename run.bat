aa5082303dbc078b02de3e7ae3ce052fefad495d
EdgardoAndres <EdgardooAndres@users.noreply.github.com>
2019-05-22 03:14:08 -0400

    [X] added requirements file.

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-"C:\Users\drgdm\AppData\Local\Programs\Python\Python37\python.exe" "C:\Users\[YOUR USERNAME]\[YOUR PATH]\EWAS_Application\main.py"
+"C:\Users\[YOUR USERNAME]\AppData\Local\Programs\Python\Python37\python.exe" "C:\Users\[YOUR USERNAME]\[YOUR PATH]\EWAS_Application\main.py"
 pause
